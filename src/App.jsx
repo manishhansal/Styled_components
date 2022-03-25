9841661fd00fcf49342f8ea3cd63df31ab5b33cf
Manish Kumar <manishhansalmeh@gmail.com>
2022-03-25 18:19:42 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
       Styled Components.
       <div>
         <Button color={"white"} backgroundColor={"#1890FF"} border={"1px dashed #1890FF"}>Primary Button</Button>
-        <Button backgroundColor={"white"} border={"1px solid black"}>Default Button</Button>
+        <Button backgroundColor={"white"} border={"1px solid #cecece"}>Default Button</Button>
         <Button border={"1px dashed #cecece"} backgroundColor={"white"}>Dashed Button</Button>
       </div>
       <Button border={"none"} backgroundColor={"white"}>Text Button</Button>
